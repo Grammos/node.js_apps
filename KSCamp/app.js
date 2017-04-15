fec7d17296b22f00faa7e15738c8c53b270ce361
Grammos <kamberigramos@gmail.com>
2017-04-15 23:34:39 +0200

    Added the header/footer in the partials folder;

---
+++ KSCamp/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+	res.render("landing.ejs");
+});
+
+app.get("/campgrounds", function(req, res){
+
+});
+
+app.listen(3000, function(){
+	console.log("Server is running on localhost:3000");
+});
