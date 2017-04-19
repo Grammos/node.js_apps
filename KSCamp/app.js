167f719d38f94768c54b85f05666e7bd1ffb0895
Grammos <kamberigramos@gmail.com>
2017-04-19 11:54:54 +0200

    Show page which would shows all the campgrounds!

--- KSCamp/app.js
+++ KSCamp/app.js
@@ -12,7 +12,8 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 
 // schema to the model
@@ -21,7 +22,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 // 	{ 
 // 		name: "Salmon Creek", 
-// 		image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"
+// 		image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg",
+// 		description: "This is a huge blah. No, O2!"
 // 	}, function(err, campground){
 // 		if(err){
 // 			console.log(err);
@@ -32,41 +34,30 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // 	});
 
 
-
-// var campgrounds = [
-//         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-//         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-//         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-//         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-//         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-//         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-//         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-//         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-//         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
-// ];
-
 app.get("/", function(req, res){
 	res.render("landing.ejs");
 });
 
-
+// INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
 	// Get all campgrounds from DB
 	Campground.find({}, function(err, allCampgrounds){
 		if(err){
 			console.log(err);
 		}else{
-			res.render("campgrounds", {campgrounds:allCampgrounds});
+			res.render("index", {campgrounds:allCampgrounds});
 		}
 	});
 });
 
+// CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res){
 	//to get the data from the form 
 	var name = req.body.name;
 	var image = req.body.image;
+	var desc = req.body.description;
 	//creating the objects in order to push to the array
-	var newCampground = {name:name, image:image}
+	var newCampground = {name:name, image:image, description:desc}
 	// create a new campground and save to the DB
 	Campground.create(newCampground, function(err, newlyCreate){
 		if(err){
@@ -79,10 +70,25 @@ app.post("/campgrounds", function(req, res){
 
 });
 
+// NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
 	res.render("new");
 });
 
+// SHOW - shows more info about one campground
+app.get("/campgrounds/:id", function(req, res){
+	//find the campground with the provided ID
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+		}else{
+		   //render show template with that campground
+	       res.render("show", {campground: foundCampground});
+
+		}
+	});
+	
+});
 
 app.listen(3000, function(){
 	console.log("Server is running on localhost:3000");
