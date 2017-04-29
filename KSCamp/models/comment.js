da9816ea3ee739d550d36c4083afe5538532e714
Grammos <kamberigramos@gmail.com>
2017-04-29 15:49:03 +0200

    Added the new comment form

--- KSCamp/models/comment.js
+++ KSCamp/models/comment.js
@@ -1,7 +1,9 @@
 var mongoose = require("mongoose");
+//SCHEMA SETUP
 var commentSchema = mongoose.Schema({
 	text: String,
 	author: String
 });
 
+// schema to the model
 module.exports = mongoose.model("Comment", commentSchema);
