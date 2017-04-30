37d0849be780b224bfb27783629a265b772c0a52
Grammos <kamberigramos@gmail.com>
2017-04-30 09:40:05 +0200

    Created mongoose Schema, and the models folder!

--- authentication/authDemo/app.js
+++ authentication/authDemo/app.js
@@ -1,6 +1,11 @@
-var express    = require("express"),
-	mongoose   = require("mongoose"),
-    app        = express();
+var express               = require("express"),
+	mongoose              = require("mongoose"),
+	passport              = require("passport"),
+	bodyParser            = require("body-parser"),
+	LocalStrategy         = require("passport-local"),
+	passportLocalMongoose = require("passport-local-mongoose"),
+	User                  = require("./models/user"),
+    app                   = express();
 
 //connect moongose to the mongodb's db
 mongoose.connect("mongodb://localhost/auth_demo_app"); 
