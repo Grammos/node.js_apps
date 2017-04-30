1aef20f1cf04774e206d1699623be2419410ac85
Grammos <kamberigramos@gmail.com>
2017-04-30 10:41:35 +0200

    Configure passport!

--- authentication/authDemo/app.js
+++ authentication/authDemo/app.js
@@ -9,13 +9,33 @@ var express               = require("express"),
 
 //connect moongose to the mongodb's db
 mongoose.connect("mongodb://localhost/auth_demo_app"); 
+
 app.set("view engine", "ejs");
+
+//express-session - other way to require besides the upper way of doing!
+app.use(require("express-session")({
+	//this would be used to encode or to decode the info
+	//in the session
+	secret: "Kosova Kosovo",
+	resave: false,
+	saveUninitialized: false
+}));
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//reading the session, taking the data from the session that's encoded
+//and uncoded and encoded again and putting them in the session again!
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 
 app.get("/", function(req, res){
 	res.render("home");
 });
-
 
 app.get("/secret", function(req, res){
 	res.render("secret");
